bca437ffd6b496a02c631174ad851af80b5fe47b
Jim Fingal <jim.fingal@gmail.com>
2014-12-06 14:36:36 -0800

    solver

---
+++ solve_puzzle.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+from twython import Twython
+import re
+import requests
+import os
+from unipath import Path
+from solver import ThesaurusSolver
+
+def get_thesaurus_text():
+    index_url = "http://www.gutenberg.org/cache/epub/10681/pg10681.txt"
+    index_text = requests.get(index_url).text
+    relevant_text = index_text[4764:2781002]
+    return relevant_text
+
+def get_tweets(twitter):
+    timeline = twitter.get_user_timeline(screen_name='ThesaurusGame')
+    return list(map(lambda x: x['text'], timeline))
+
+def get_hint_and_related(tweet):
+    extract_data = re.compile('Hint: (?P<hint>.+)\\nRelated words are: (?P<related>.+)\.', re.IGNORECASE)
+    matches = extract_data.search(tweet)
+    
+    print tweet
+    
+    hint = matches.groupdict()['hint']
+    hint = hint.encode('utf-8').replace('●', '.').replace(' ', '')
+    hint_regex = ';(?P<name>' + hint + ');'    
+    
+    match_regex = re.compile(hint_regex, re.IGNORECASE)
+    related = matches.groupdict()['related'].split(', ')
+    
+    return match_regex, related, hint_regex
+
+def get_and_solve_tweet(solver):
+    tweets = get_tweets()
+    last_tweet = tweets[0]
+    print "Last weet was: %s" % last_tweet 
+    if 'Guess the word' in last_tweet:
+        try:
+            match_regex, related, hint_regex = get_hint_and_related(last_tweet)
+            print match_regex
+            print related
+            print hint_regex
+            answers =  solver.solve_problem(match_regex, related)
+            if len(answers) > 0:
+                return answers[0][0]
+            else:
+                return None
+        except Exception as e:
+            print e
+    else:
+        print "No current Challenge: %s" % last_tweet
+
+if __name__ == "__main__":
+
+    consumer_key = os.environ.get('GENIUS_CONSUMER_KEY')
+    consumer_secret = os.environ.get('GENIUS_CONSUMER_SECRET')
+    access_token = os.environ.get('GENIUS_ACCESS_TOKEN')
+    access_token_secret = os.environ.get('GENIUS_ACCESS_TOKEN_SECRET')
+
+    twitter = Twython(consumer_key,
+                          consumer_secret,
+                          access_token,
+                          access_token_secret)
+
+
+    p = Path('thesaurus.txt')
+    text = None
+    if not p.exists():
+        print "File doesn't exist, parsing from internet"
+        text = get_thesaurus_text()
+        p.write_file(text.encode('utf-8'))
+    else:
+        print "File exists, reading in"
+        text = p.read_file().decode('utf-8')
+
+    solver = ThesaurusSolver(text)
+    answers = get_and_solve_tweet(solver)
+    print answers
---
+++ solver.py
@@ -0,0 +1,53 @@
+from collections import defaultdict, Counter
+
+class ThesaurusSolver(object):
+    
+    def __init__(self, thesaurus):
+        self.forward_mapping, self.reverse_mapping, self.matchable_text = self.process_thesaurus(thesaurus)
+        
+    def process_thesaurus(self, thesaurus):
+    
+        forward_mapping = defaultdict(set)
+        reverse_mapping = defaultdict(set)
+        
+        all_words = set()
+                
+        current_word = None
+
+        for i, line in enumerate(thesaurus.split('\r\n')):
+            if '        ' in line:
+                without_number = ' '.join(line.replace('        ', '').split()[:-1])
+                multiple_words = without_number.split(',')
+                for word in multiple_words:
+                    forward_mapping[current_word].add(word)
+                    reverse_mapping[word].add(current_word)
+            else:
+                current_word = line.lower().strip()
+                all_words.add(current_word)
+
+        matchable_text = ';'.join(all_words) + ';'
+        
+        return forward_mapping, reverse_mapping, matchable_text
+        
+
+    def solve_problem(self, match_regex, hints):
+        
+        print match_regex, hints
+        
+        matches = match_regex.findall(self.matchable_text)
+
+        score = Counter()
+        print matches
+        for match in matches:
+            for word in self.forward_mapping[match]:
+                #if word in hints:
+                #    score[match] += 1
+                for reverse in self.reverse_mapping[word]:
+                    if reverse in hints:
+                        score[match] += 1
+                        
+        possible_words = []
+        for word, score in score.most_common():
+            if word not in hints:
+                possible_words.append((word, score))
+        return possible_words
