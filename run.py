12c39891d7cd449cc358110580b7c436ec387fb2
Jim Fingal <jim.fingal@gmail.com>
2014-12-06 17:30:28 -0800

    solve interval to environ, a little comments, rearrange imports

--- run.py
+++ run.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 import re
-
-import arrow
+import os
 import time
 import logging
+
+import arrow
 
 from twitterhelpers import get_twython, get_tweets, post_solution
 from thesaurus import get_thesaurus_text
 from solver import ThesaurusSolver
 
-SLEEP_MIN = 45
+SLEEP_MIN = os.environ.get('SOLVE_INTERVAL', 45)
 
 log_fmt = "%(levelname)-6s %(filename)-12s:%(lineno)-4d at %(asctime)s: %(message)s"
 logging.basicConfig(level=logging.DEBUG, format=log_fmt)
-
 
 def main_loop(twitter, solver):
     # Once per human-reasonable hour, attempt to solve.
@@ -69,17 +69,21 @@ def get_and_solve_tweet(twitter, solver):
         logging.info("No current Challenge: %s" % last_tweet)
 
 def get_hint_and_related(tweet):
+
+    # Pull out the hint and related text
     extract_data = re.compile('.*int. (?P<hint>.+)\n.+words are: (?P<related>.+)[\.]*', re.IGNORECASE)
     matches = extract_data.search(tweet)
         
+    # Turn the hint into regex
     hint = matches.groupdict()['hint']
     hint = hint.encode('utf-8').replace('●', '.').replace(' ', '')
-    hint_regex = ';(?P<name>' + hint + ');'    
-    
-    match_regex = re.compile(hint_regex, re.IGNORECASE)
+    hint_regex_text = ';(?P<name>' + hint + ');'    
+    match_regex = re.compile(hint_regex_text, re.IGNORECASE)
+
+    # Get related words
     related = matches.groupdict()['related'].split(', ')
     
-    return match_regex, related, hint_regex
+    return match_regex, related, hint_regex_text
 
 if __name__ == "__main__":
     
